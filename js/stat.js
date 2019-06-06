98e74eff831f78b276c4fbe585390c65dff9ce19
a20150209 <a20150209@gmail.com>
2019-06-06 22:18:07 +0300

    Черновая версия готова

--- js/stat.js
+++ js/stat.js
@@ -4,16 +4,28 @@ window.renderStatistics = function (ctx, names, times) {
         x: 100,
         y: 10,
     };
-
     var cloudShadowCoodinates = {
         x: cloudCoordinates.x + 10,
         y: cloudCoordinates.y + 10
     };
+    var cloudColor = 'rgba(255, 255, 255, 1)';
+    var cloudShadowColor = 'rgba(0, 0, 0, 0.7)';
+    var mainColor = 'rgba(0, 0, 0, 1)';
+    
+    var histogramHeight = 150;
+    var histogramColumnWidth = 40;
+    var histogramColumnPadding = 50;
+    var histogramUserColor = 'rgba(255, 0, 0, 1)';
+    var histogramUserName = 'Вы';
+    var histogramColumCoordinates = {
+        x: cloudCoordinates.x + 50,
+        y: cloudCoordinates.y + 230
+    };   
 
     function createHorizontalSide(direction, object) {
         var width = 420;
         var xPixelsInStep = 15;
-        var yPixelsInStep = -10;
+        var yPixelsInStep = -5;
         var shiftX = xPixelsInStep
         var shiftY = yPixelsInStep
         var x = object.x;
@@ -42,7 +54,7 @@ window.renderStatistics = function (ctx, names, times) {
 
     function createVerticalSide(direction, object) {
         var height = 270;
-        var xPixelsInStep = -10;
+        var xPixelsInStep = -5;
         var yPixelsInStep = 15;
         var shiftX = xPixelsInStep;
         var shiftY = yPixelsInStep;
@@ -73,8 +85,7 @@ window.renderStatistics = function (ctx, names, times) {
     function paintObject(object, color) {
         ctx.beginPath();
         ctx.moveTo(object.x, object.y);
-
-
+        
         createHorizontalSide('right', object);
         createVerticalSide('bottom', object);
         createHorizontalSide('left', object);
@@ -85,14 +96,38 @@ window.renderStatistics = function (ctx, names, times) {
         ctx.fill();
     };
 
-    paintObject(cloudShadowCoodinates, 'rgba(0, 0, 0, 0.7)')
-    paintObject(cloudCoordinates, 'rgba(255, 255, 255, 1)')
+    paintObject(cloudShadowCoodinates,);
+    paintObject(cloudCoordinates, cloudColor);
 
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillStyle = mainColor;
     ctx.font = '16px PT Mono';
-    ctx.fillText('Ура вы победили!', cloudCoordinates.x + 40, cloudCoordinates.y + 40);
-     ctx.fillText('Список результатов:', cloudCoordinates.x + 40, cloudCoordinates.y + 60);
+    ctx.fillText('Ура вы победили!', cloudCoordinates.x + 40, cloudCoordinates.y + 20);
+    ctx.fillText('Список результатов:', cloudCoordinates.x + 40, cloudCoordinates.y + 40);
     
     
+    var maxTime = times[0];
+    for (var i = 0; i < times.length; i++) {
+        times[i] = Math.round(times[i]);
+        if (times[i] > maxTime) {
+            maxTime = times[i];
+            var maxTimeElementIndex = i;
+        };
+    };
     
+    for (var i = 0; i < names.length; i++) {
+        if (names[i] === histogramUserName) {
+            ctx.fillStyle = histogramUserColor;
+        } else {
+            var histogramOtherUsersColor = 'rgba(0, 0, 255, ' + Math.random(0, 1) + ')';
+            ctx.fillStyle = histogramOtherUsersColor;
+        };
+        
+        var histogramUserHeight = histogramHeight / maxTime * times[i];
+        ctx.fillRect(histogramColumCoordinates.x, histogramColumCoordinates.y, histogramColumnWidth, -histogramUserHeight);
+        
+        ctx.fillStyle = mainColor;
+        ctx.fillText(times[i], histogramColumCoordinates.x, histogramColumCoordinates.y - 160);
+        ctx.fillText(names[i], histogramColumCoordinates.x, histogramColumCoordinates.y + 30);
+        histogramColumCoordinates.x += histogramColumnPadding + histogramColumnWidth;
+    };
 };
