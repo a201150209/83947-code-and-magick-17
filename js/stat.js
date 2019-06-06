a1da1c27e0d353e700194e04a83717481262b70b
a20150209 <a20150209@gmail.com>
2019-06-06 22:45:08 +0300

    Навел немного красоты

--- js/stat.js
+++ js/stat.js
@@ -1,133 +1,136 @@
+'use strict';
+
 window.renderStatistics = function (ctx, names, times) {
+  var cloudCoordinates = {
+    x: 100,
+    y: 10
+  };
 
-    var cloudCoordinates = {
-        x: 100,
-        y: 10,
-    };
-    var cloudShadowCoodinates = {
-        x: cloudCoordinates.x + 10,
-        y: cloudCoordinates.y + 10
-    };
-    var cloudColor = 'rgba(255, 255, 255, 1)';
-    var cloudShadowColor = 'rgba(0, 0, 0, 0.7)';
-    var mainColor = 'rgba(0, 0, 0, 1)';
-    
-    var histogramHeight = 150;
-    var histogramColumnWidth = 40;
-    var histogramColumnPadding = 50;
-    var histogramUserColor = 'rgba(255, 0, 0, 1)';
-    var histogramUserName = 'Вы';
-    var histogramColumCoordinates = {
-        x: cloudCoordinates.x + 50,
-        y: cloudCoordinates.y + 230
-    };   
+  var cloudShadowCoodinates = {
+    x: cloudCoordinates.x + 10,
+    y: cloudCoordinates.y + 10
+  };
 
-    function createHorizontalSide(direction, object) {
-        var width = 420;
-        var xPixelsInStep = 15;
-        var yPixelsInStep = -5;
-        var shiftX = xPixelsInStep
-        var shiftY = yPixelsInStep
-        var x = object.x;
-        var y = object.y;
+  var cloudColor = 'rgba(255, 255, 255, 1)';
+  var cloudShadowColor = 'rgba(0, 0, 0, 0.7)';
+  var mainColor = 'rgba(0, 0, 0, 1)';
+  var histogramHeight = 150;
+  var histogramColumnWidth = 40;
+  var histogramColumnPadding = 50;
+  var histogramUserColor = 'rgba(255, 0, 0, 1)';
+  var histogramUserName = 'Вы';
 
-        if (direction === 'left') {
-            shiftX = -xPixelsInStep;
-            shiftY = -yPixelsInStep
-        };
+  var histogramColumnCoordinates = {
+    x: cloudCoordinates.x + 60,
+    y: cloudCoordinates.y + 230
+  };
 
-        for (var i = 0; i <= (width / xPixelsInStep); i++) {
-            if (i !== 0 && i % 2 === 0) {
-                x += shiftX;
-                y += shiftY;
-            } else if (i !== 0) {
-                x += shiftX;
-                y -= shiftY;
-            };
+  function createHorizontalSide(direction, object) {
+    var width = 420;
+    var xPixelsInStep = 15;
+    var yPixelsInStep = -5;
+    var shiftX = xPixelsInStep;
+    var shiftY = yPixelsInStep;
+    var x = object.x;
+    var y = object.y;
 
-            ctx.lineTo(x, y);
-        };
+    if (direction === 'left') {
+      shiftX = -xPixelsInStep;
+      shiftY = -yPixelsInStep;
+    }
 
-        object.x = x;
-        object.y = y;
+    for (var i = 0; i <= (width / xPixelsInStep); i++) {
+      if (i !== 0 && i % 2 === 0) {
+        x += shiftX;
+        y += shiftY;
+      } else if (i !== 0) {
+        x += shiftX;
+        y -= shiftY;
+      };
+
+      ctx.lineTo(x, y);
     };
 
-    function createVerticalSide(direction, object) {
-        var height = 270;
-        var xPixelsInStep = -5;
-        var yPixelsInStep = 15;
-        var shiftX = xPixelsInStep;
-        var shiftY = yPixelsInStep;
-        var x = object.x;
-        var y = object.y;
+    object.x = x;
+    object.y = y;
+  };
 
-        if (direction === 'top') {
-            shiftY = -yPixelsInStep;
-            shiftX = -xPixelsInStep;
-        };
+  function createVerticalSide(direction, object) {
+    var height = 270;
+    var xPixelsInStep = -5;
+    var yPixelsInStep = 15;
+    var shiftX = xPixelsInStep;
+    var shiftY = yPixelsInStep;
+    var x = object.x;
+    var y = object.y;
 
-        for (var i = 0; i <= (height / yPixelsInStep); i++) {
-            if (i !== 0 && i % 2 === 0) {
-                x -= shiftX;
-                y += shiftY;
-            } else if (i !== 0) {
-                x += shiftX;
-                y += shiftY;
-            };
-
-            ctx.lineTo(x, y);
-        };
-
-        object.x = x;
-        object.y = y;
+    if (direction === 'top') {
+      shiftY = -yPixelsInStep;
+      shiftX = -xPixelsInStep;
     };
 
-    function paintObject(object, color) {
-        ctx.beginPath();
-        ctx.moveTo(object.x, object.y);
-        
-        createHorizontalSide('right', object);
-        createVerticalSide('bottom', object);
-        createHorizontalSide('left', object);
-        createVerticalSide('top', object);
+    for (var i = 0; i <= (height / yPixelsInStep); i++) {
+      if (i !== 0 && i % 2 === 0) {
+        x -= shiftX;
+        y += shiftY;
+      } else if (i !== 0) {
+        x += shiftX;
+        y += shiftY;
+      };
 
-        ctx.closePath();
-        ctx.fillStyle = color;
-        ctx.fill();
+      ctx.lineTo(x, y);
     };
 
-    paintObject(cloudShadowCoodinates,);
-    paintObject(cloudCoordinates, cloudColor);
+    object.x = x;
+    object.y = y;
+  };
+
+  function paintObject(object, color) {
+    ctx.beginPath();
+    ctx.moveTo(object.x, object.y);
+
+    createHorizontalSide('right', object);
+    createVerticalSide('bottom', object);
+    createHorizontalSide('left', object);
+    createVerticalSide('top', object);
+
+    ctx.closePath();
+    ctx.fillStyle = color;
+    ctx.fill();
+  };
+
+  paintObject(cloudShadowCoodinates, );
+  paintObject(cloudCoordinates, cloudColor);
+
+  ctx.fillStyle = mainColor;
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', cloudCoordinates.x + 60, cloudCoordinates.y + 20);
+  ctx.fillText('Список результатов:', cloudCoordinates.x + 60, cloudCoordinates.y + 40);
+
+
+  var maxTime = times[0];
+  for (var i = 0; i < times.length; i++) {
+    times[i] = Math.round(times[i]);
+    if (times[i] > maxTime) {
+      maxTime = times[i];
+      var maxTimeElementIndex = i;
+    };
+  };
+
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] === histogramUserName) {
+      ctx.fillStyle = histogramUserColor;
+    } else {
+      var histogramOtherUsersColor = 'rgba(0, 0, 255, ' + Math.random(0.5, 1) + ')';
+      ctx.fillStyle = histogramOtherUsersColor;
+    };
+
+    var histogramUserHeight = histogramHeight / maxTime * times[i];
+    ctx.fillRect(histogramColumnCoordinates.x, histogramColumnCoordinates.y, histogramColumnWidth, -histogramUserHeight);
 
     ctx.fillStyle = mainColor;
-    ctx.font = '16px PT Mono';
-    ctx.fillText('Ура вы победили!', cloudCoordinates.x + 40, cloudCoordinates.y + 20);
-    ctx.fillText('Список результатов:', cloudCoordinates.x + 40, cloudCoordinates.y + 40);
-    
-    
-    var maxTime = times[0];
-    for (var i = 0; i < times.length; i++) {
-        times[i] = Math.round(times[i]);
-        if (times[i] > maxTime) {
-            maxTime = times[i];
-            var maxTimeElementIndex = i;
-        };
-    };
-    
-    for (var i = 0; i < names.length; i++) {
-        if (names[i] === histogramUserName) {
-            ctx.fillStyle = histogramUserColor;
-        } else {
-            var histogramOtherUsersColor = 'rgba(0, 0, 255, ' + Math.random(0, 1) + ')';
-            ctx.fillStyle = histogramOtherUsersColor;
-        };
-        
-        var histogramUserHeight = histogramHeight / maxTime * times[i];
-        ctx.fillRect(histogramColumCoordinates.x, histogramColumCoordinates.y, histogramColumnWidth, -histogramUserHeight);
-        
-        ctx.fillStyle = mainColor;
-        ctx.fillText(times[i], histogramColumCoordinates.x, histogramColumCoordinates.y - 160);
-        ctx.fillText(names[i], histogramColumCoordinates.x, histogramColumCoordinates.y + 30);
-        histogramColumCoordinates.x += histogramColumnPadding + histogramColumnWidth;
-    };
+    ctx.fillText(times[i], histogramColumnCoordinates.x, histogramColumnCoordinates.y - 160);
+    ctx.fillText(names[i], histogramColumnCoordinates.x, histogramColumnCoordinates.y + 30);
+    histogramColumnCoordinates.x += histogramColumnPadding + histogramColumnWidth;
+  };
 };
